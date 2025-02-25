20c567377928ac090ea5c8421dcded7a21fd1063
David <david.moran@alu.uclm.es>
2025-02-25 12:17:06 +0100

    Implement new feature for enhanced logging in partition management

--- main/main.c
+++ main/main.c
@@ -2,15 +2,16 @@
 
 #include "EKF.h"
 #include "buffer.h"
+#include "driver/gpio.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_partition.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "i2cdev.h"
-#include "logging.h"
 #include "math.h"
 #include "mpu9250.h"
+#include "partitionLog.h"
 #include "string.h"
 
 #define CHECK(x)                       \
@@ -19,12 +20,14 @@
     if ((__ = x) != ESP_OK) return __; \
   } while (0)
 
+#define APP_INIT_BUTTON_PIN GPIO_NUM_23
+
 #define I2C_PORT 0
 #define I2C_SDA 33
 #define I2C_SCL 32
 
-#define MEASURES_BUFFER_ITEM_NUMBER 20
-#define RESULTS_BUFFER_ITEM_NUMBER 20
+#define MEASURES_BUFFER_ITEM_NUMBER 100
+#define LOG_BUFFER_ITEM_NUMBER 100
 
 #define US_TO_S ((float)(1.f / 1000000.f));
 
@@ -86,12 +89,18 @@ typedef struct {
 
 typedef struct {
   vectorBuffer_t *measuresBuffer;
-  vectorBuffer_t *resultsBuffer;
+  vectorBuffer_t *logBuffer;
   mpu9250_dev_t *mpu9250;
   EKF_ctx_t *ekf;
   TickType_t taskPeriod;
 
 } measurementsProcesorCfg_t;
+
+typedef struct {
+  vectorBuffer_t *logBuffer;
+  logPartition_t *logPartition;
+  TickType_t taskPeriod;
+} loggerCfg_t;
 
 /**
  * Function declarations
@@ -108,23 +117,35 @@ esp_err_t apply_mpu9250_calibration(mpu9250_dev_t *dev,
 
 void measurementsProducer(void *param);
 void measurementsProcesor(void *param);
-void logger();
+void logger(void *param);
 
 /**
  * Function definitions
  *
  */
 
+void lockUntilPress(void) {
+  gpio_reset_pin(APP_INIT_BUTTON_PIN);
+  gpio_set_direction(APP_INIT_BUTTON_PIN, GPIO_MODE_INPUT);
+  gpio_set_pull_mode(APP_INIT_BUTTON_PIN, GPIO_PULLUP_ONLY);
+  fprintf(stdout, "Waiting for button press.\n");
+  while (gpio_get_level(APP_INIT_BUTTON_PIN) == 1) {
+    vTaskDelay(pdMS_TO_TICKS(10));
+  }
+}
+
 esp_err_t app_init(mpu9250_dev_t *dev, EKF_ctx_t *ekf,
                    vectorBuffer_t *measuresBuffer,
-                   vectorBuffer_t *resultsBuffer) {
+                   vectorBuffer_t *logBuffer) {
   const char *TAG = "APP_INIT";
   esp_log_level_set(TAG, ESP_LOG_DEBUG);
 
   configASSERT(dev);
   configASSERT(measuresBuffer);
-  configASSERT(resultsBuffer);
+  configASSERT(logBuffer);
   configASSERT(ekf);
+
+  lockUntilPress();
 
   CHECK(i2cdev_init());
   ESP_LOGI(TAG, "I2C init success");
@@ -139,6 +160,8 @@ esp_err_t app_init(mpu9250_dev_t *dev, EKF_ctx_t *ekf,
   CHECK(mpu9250_init(dev));
   ESP_LOGI(TAG, "MPU9250 init success");
 
+  CHECK(mpu6050_set_dlpf_mode(dev, MPU6050_DLPF_2));
+
   calibrationData_t calibration = {0};
   CHECK(read_mpu9250_calibration(&calibration));
   for (uint8_t i = 0; i < 9; i++) {
@@ -148,16 +171,16 @@ esp_err_t app_init(mpu9250_dev_t *dev, EKF_ctx_t *ekf,
   for (uint8_t i = 0; i < 3; i++) {
     ESP_LOGD(TAG, "b Cal Data %u: %.2f", i, calibration.mag.b[i]);
   }
-  
+
   CHECK(apply_mpu9250_calibration(dev, &calibration));
   ESP_LOGI(TAG, "MPU9250 calibration success");
 
   CHECK(initBuffer(measuresBuffer, sizeof(measureItem_t),
                    MEASURES_BUFFER_ITEM_NUMBER));
   ESP_LOGI(TAG, "Measurements buffer init success");
-  CHECK(initBuffer(resultsBuffer, sizeof(resultItem_t),
-                   RESULTS_BUFFER_ITEM_NUMBER));
-  ESP_LOGI(TAG, "Results buffer init success");
+  CHECK(initBuffer(logBuffer, sizeof(logData_t),
+                   LOG_BUFFER_ITEM_NUMBER));
+  ESP_LOGI(TAG, "Log buffer init success");
 
   measures_t initMeasures;
   mpu9250_get_motion(dev, initMeasures.acc, initMeasures.velAng,
@@ -166,8 +189,6 @@ esp_err_t app_init(mpu9250_dev_t *dev, EKF_ctx_t *ekf,
   ESP_LOGI(TAG, "EKF init success");
 
   ESP_LOGI(TAG, "Free heap size: %lu KB", esp_get_free_heap_size() / 1000);
-
-  vTaskDelay(200);
 
   return ESP_OK;
 }
@@ -248,7 +269,7 @@ void measurementsProducer(void *param) {
 
 void measurementsProcesor(void *param) {
   char *TAG = "PROCESOR_TASK";
-  esp_log_level_set(TAG, ESP_LOG_DEBUG);
+  esp_log_level_set(TAG, ESP_LOG_INFO);
   ESP_LOGI(TAG, "Task Init");
 
   measurementsProcesorCfg_t *conf = param;
@@ -257,7 +278,7 @@ void measurementsProcesor(void *param) {
   measureItem_t measureItem;
   measures_t ekfMeasures;
   float mag_norm = 0;
-  resultItem_t resultItem;
+  logData_t logItem = {0};
   esp_err_t err;
 
   while (1) {
@@ -266,11 +287,12 @@ void measurementsProcesor(void *param) {
       err = pullItem(conf->measuresBuffer, &measureItem, sizeof(measureItem));
       if (err == ESP_ERR_TIMEOUT) break;
       if (err != ESP_OK) {
-        ESP_LOGW(TAG, "Buffer pull exception: %s", esp_err_to_name(err));
+        ESP_LOGW(TAG, "Buffer measures pull exception: %s",
+                 esp_err_to_name(err));
         vTaskDelay(1);
         continue;
       }
-      
+
       ekfMeasures.acc[0] = measureItem.acc.x;
       ekfMeasures.acc[1] = measureItem.acc.y;
       ekfMeasures.acc[2] = measureItem.acc.z;
@@ -279,18 +301,27 @@ void measurementsProcesor(void *param) {
       ekfMeasures.mag[1] = measureItem.mag.y;
       ekfMeasures.mag[2] = measureItem.mag.z;
 
-      ekfMeasures.velAng[0] = measureItem.gyro.x-0.04;
-      ekfMeasures.velAng[1] = measureItem.gyro.y+0.04;
-      ekfMeasures.velAng[2] = measureItem.gyro.z+0.04;
+      ekfMeasures.velAng[0] = measureItem.gyro.x - 0.045;
+      ekfMeasures.velAng[1] = measureItem.gyro.y + 0.038;
+      ekfMeasures.velAng[2] = measureItem.gyro.z + 0.045;
 
       ekfStep(conf->ekf, &ekfMeasures, measureItem.measureTime);
 
-      resultItem.measureTime = measureItem.measureTime;
+      logItem.timestamp = measureItem.measureTime;
 
-      resultItem.quat[0] = gsl_quat_float_get(conf->ekf->q_current, 0);
-      resultItem.quat[1] = gsl_quat_float_get(conf->ekf->q_current, 1);
-      resultItem.quat[2] = gsl_quat_float_get(conf->ekf->q_current, 2);
-      resultItem.quat[3] = gsl_quat_float_get(conf->ekf->q_current, 3);
+      logItem.quat[0] = gsl_quat_float_get(conf->ekf->q_current, 0);
+      logItem.quat[1] = gsl_quat_float_get(conf->ekf->q_current, 1);
+      logItem.quat[2] = gsl_quat_float_get(conf->ekf->q_current, 2);
+      logItem.quat[3] = gsl_quat_float_get(conf->ekf->q_current, 3);
+      logItem.acc[0] = measureItem.acc.x;
+      logItem.acc[1] = measureItem.acc.y;
+      logItem.acc[2] = measureItem.acc.z;
+      logItem.gyro[0] = ekfMeasures.velAng[0];
+      logItem.gyro[1] = ekfMeasures.velAng[1];
+      logItem.gyro[2] = ekfMeasures.velAng[2];
+      logItem.mag[0] = measureItem.mag.x;
+      logItem.mag[1] = measureItem.mag.y;
+      logItem.mag[2] = measureItem.mag.z;
 
       // Log accelerometer values
       ESP_LOGD(TAG, "Accelerometer values: X: %.2f, Y: %.2f, Z: %.2f",
@@ -302,24 +333,85 @@ void measurementsProcesor(void *param) {
 
       // Log angular velocity values
       ESP_LOGD(TAG, "Angular velocity values: X: %.2f, Y: %.2f, Z: %.2f",
-        ekfMeasures.velAng[0], ekfMeasures.velAng[1], ekfMeasures.velAng[2]);
-      ESP_LOGI(TAG, "t=%.4f, q0=%.2f, qx=%.2f, qy=%.2f, qz=%.2f",
-               resultItem.measureTime, resultItem.quat[0], resultItem.quat[1],
-               resultItem.quat[2], resultItem.quat[3]);
-      //   do {
-      //   err = pushItem(conf->resultsBuffer, &resultItem, sizeof(resultItem));
-      //   if (err != ESP_OK) {
-      //     ESP_LOGW(TAG, "Buffer push exception: %s",
-      //              esp_err_to_name(err));
-      //     vTaskDelay(1);
-      //   }
-      // } while (err != ESP_OK);
+               ekfMeasures.velAng[0], ekfMeasures.velAng[1],
+               ekfMeasures.velAng[2]);
+      ESP_LOGD(TAG, "t=%.4f, q0=%.2f, qx=%.2f, qy=%.2f, qz=%.2f",
+               logItem.timestamp, logItem.quat[0], logItem.quat[1],
+               logItem.quat[2], logItem.quat[3]);
+      do {
+        err = pushItem(conf->logBuffer, &logItem, sizeof(logItem));
+        if (err != ESP_OK) {
+          ESP_LOGW(TAG, "Buffer results push exception: %s",
+                   esp_err_to_name(err));
+          vTaskDelay(1);
+        }
+      } while (err != ESP_OK);
 
     } while (err != ESP_ERR_TIMEOUT);
 
     if (xTaskDelayUntil(&prevTick, conf->taskPeriod) == pdFALSE) {
-      ESP_LOGW(TAG, "Missed %ld ticks",
-               (prevTick - prevPrevTick) - conf->taskPeriod);
+      // ESP_LOGW(TAG, "Missed %ld ticks",
+      //          (prevTick - prevPrevTick) - conf->taskPeriod);
+    }
+  }
+}
+
+void logger(void *param) {
+  const char *TAG = "LOGGER_TASK";
+  esp_log_level_set(TAG, ESP_LOG_DEBUG);
+  ESP_LOGI(TAG, "Task Init");
+
+  loggerCfg_t *conf = param;
+
+  TickType_t prevTick = 0, prevPrevTick = 0;
+  logData_t logItem;
+  esp_err_t err;
+
+  while (1) {
+    prevPrevTick = prevTick;
+    do {
+      err = pullItem(conf->logBuffer, &logItem, sizeof(logItem));
+      if (err == ESP_ERR_TIMEOUT) break;
+      if (err != ESP_OK) {
+        ESP_LOGW(TAG, "Buffer pull exception: %s", esp_err_to_name(err));
+        vTaskDelay(1);
+        continue;
+      }
+
+      conf->logPartition->data.timestamp = logItem.timestamp;
+      conf->logPartition->data.quat[0] = logItem.quat[0];
+      conf->logPartition->data.quat[1] = logItem.quat[1];
+      conf->logPartition->data.quat[2] = logItem.quat[2];
+      conf->logPartition->data.quat[3] = logItem.quat[3];
+      conf->logPartition->data.acc[0] = logItem.acc[0];
+      conf->logPartition->data.acc[1] = logItem.acc[1];
+      conf->logPartition->data.acc[2] = logItem.acc[2];
+      conf->logPartition->data.gyro[0] = logItem.gyro[0];
+      conf->logPartition->data.gyro[1] = logItem.gyro[1];
+      conf->logPartition->data.gyro[2] = logItem.gyro[2];
+      conf->logPartition->data.mag[0] = logItem.mag[0];
+      conf->logPartition->data.mag[1] = logItem.mag[1];
+      conf->logPartition->data.mag[2] = logItem.mag[2];
+
+      do {
+        err = logPartitionUpdate(conf->logPartition);
+        if (err != ESP_OK) {
+          ESP_LOGW(TAG, "Log partition update exception: %s",
+                   esp_err_to_name(err));
+          vTaskDelay(1);
+        }
+      } while (err != ESP_OK);
+
+    } while (1);
+
+    logPartitionUpdateHeader(conf->logPartition);
+    ESP_LOGI(TAG,"Current Quat at time %.4f: %.4f, %.4f, %.4f, %.4f",
+             logItem.timestamp, logItem.quat[0], logItem.quat[1], logItem.quat[2],
+             logItem.quat[3]);
+
+    if (xTaskDelayUntil(&prevTick, conf->taskPeriod) == pdFALSE) {
+      // ESP_LOGW(TAG, "Missed %ld ticks",
+      //          (prevTick - prevPrevTick) - conf->taskPeriod);
     }
   }
 }
@@ -333,37 +425,51 @@ void app_main(void) {
   mpu9250_dev_t dev = {0};
   EKF_ctx_t ekf = {0};
   vectorBuffer_t measuresBuffer = {0};
-  vectorBuffer_t resultsBuffer = {0};
-  ESP_ERROR_CHECK(app_init(&dev, &ekf, &measuresBuffer, &resultsBuffer));
+  vectorBuffer_t logBuffer = {0};
+  ESP_ERROR_CHECK(app_init(&dev, &ekf, &measuresBuffer, &logBuffer));
+
+  loggerCfg_t loggerCfg = {
+      .logBuffer = &logBuffer,
+      .logPartition = logPartitionNew(),
+      .taskPeriod = pdMS_TO_TICKS(500),
+  };
+
+  configASSERT(loggerCfg.logPartition);
+  TaskHandle_t *loggerTaskHandle;
+  xTaskCreatePinnedToCore(logger, "Logger", configMINIMAL_STACK_SIZE * 10, &loggerCfg,
+              tskIDLE_PRIORITY, &loggerTaskHandle, 0);
+  configASSERT(loggerTaskHandle);
+  vTaskSuspend(loggerTaskHandle);
 
   measurementsProducerCfg_t producerCfg = {
       .buffer = &measuresBuffer,
       .mpu9250 = &dev,
-      .taskPeriod = pdMS_TO_TICKS(50),
+      .taskPeriod = pdMS_TO_TICKS(100),
   };
 
   TaskHandle_t *producerTaskHandle;
-  xTaskCreate(measurementsProducer, "Producer", configMINIMAL_STACK_SIZE * 10,
-              &producerCfg, tskIDLE_PRIORITY, &producerTaskHandle);
+  xTaskCreatePinnedToCore(measurementsProducer, "Producer", configMINIMAL_STACK_SIZE * 10,
+              &producerCfg, tskIDLE_PRIORITY, &producerTaskHandle, 0);
   configASSERT(producerTaskHandle);
   vTaskSuspend(producerTaskHandle);
 
   measurementsProcesorCfg_t procesorCfg = {
       .ekf = &ekf,
       .measuresBuffer = &measuresBuffer,
-      .resultsBuffer = &resultsBuffer,
+      .logBuffer = &logBuffer,
       .mpu9250 = &dev,
-      .taskPeriod = pdMS_TO_TICKS(500),
+      .taskPeriod = pdMS_TO_TICKS(200),
   };
 
   TaskHandle_t *procesorTaskHandle;
-  xTaskCreate(measurementsProcesor, "Procesor", configMINIMAL_STACK_SIZE * 10,
-              &procesorCfg, tskIDLE_PRIORITY, &procesorTaskHandle);
+  xTaskCreatePinnedToCore(measurementsProcesor, "Procesor", configMINIMAL_STACK_SIZE * 10,
+              &procesorCfg, tskIDLE_PRIORITY, &procesorTaskHandle, 1);
   configASSERT(procesorTaskHandle);
   vTaskSuspend(procesorTaskHandle);
 
   vTaskResume(producerTaskHandle);
   vTaskResume(procesorTaskHandle);
+  vTaskResume(loggerTaskHandle);
 
   vTaskSuspend(NULL);
 }
